5db0587a73a75c525107f69994a44fff1725b11f
Ryan Johnson <ryan@echodistrict.com>
2016-01-25 16:44:42 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+var cache = require('gulp-cache');
+var browserSync = require('browser-sync');
+
+var useref = require('gulp-useref');
+// Other requires...
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+var imagemin = require('gulp-imagemin');
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/styles.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Other watchers
+});
+
+
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    },
+  })
+});
+
+
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
+
+
+
+
+
+
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
